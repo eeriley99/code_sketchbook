85ce144bd2404864c9e4b1afd0c3bc870da54925
Erin E. Riley <53475374+eeriley99@users.noreply.github.com>
2019-08-01 16:36:56 -0400

    Update sketch.js

--- eye_picture/sketch.js
+++ eye_picture/sketch.js
@@ -1,31 +1,29 @@
 //eye by eer 8/1/19
 //demonstrates using shapes to draw a static picture
+//GH:eeriley99.github.io/code_sketchbook/eye_picture/
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(250, 250);
   background(255);
-
 }
 
 function draw() {
-  
   //eye
   strokeWeight(1);
   fill(0);
-  ellipse(width / 2, height / 2, 150, 150);
+  ellipse(width / 2, height / 2, 75, 75);
   noFill();
-  ellipse(width / 2, height / 2, 250, 250);
+  ellipse(width / 2, height / 2, 125, 125);
   rectMode(CENTER);
-  
+
   //frame
   strokeWeight(4);
-  rect(width / 2, height / 2, 300, 300);
-  rect(width / 2, height / 2, 398, 398);
-  
-  strokeWeight(1);
-  line(0, 0, 50, 50);
-  line(350, 50, 400, 0);
-  line(0, 400, 50, 350);
-  line(350, 350, 400, 400);
+  rect(width / 2, height / 2, 175, 175);
+  rect(width / 2, height / 2, 246, 246);
 
+  strokeWeight(1);
+  line(0, 0, 37.5, 37.5);
+  line(212.5, 37.5, 250, 0);
+  line(0, 250, 37.5, 212.5);
+  line(212.5, 212.5, 250, 250);
 }
